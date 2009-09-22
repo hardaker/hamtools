6e07e236ba4d1ce444fabe9e4d0fdf38fc21ba31
hardaker <hardaker@b83971f2-991a-4c8c-b955-6c2ff906e3b9>
2009-09-22 02:57:31 +0000

    r10108@wjh:  hardaker | 2008-05-23 07:59:14 -0700  Lots of GUI improvements
    git-svn-id: file:///tmp/svktogit.16720/svnrepo-hamtools.git/trunk@24 b83971f2-991a-4c8c-b955-6c2ff906e3b9

--- MyScanner.pm
+++ MyScanner.pm
@@ -26,8 +26,46 @@ my $mainpanel;
 
 sub channel_button {
     my ($channelb, $event) = @_;
-    main::set_channel($channelb->{'channel'});
-    $main::locked = 1;
+    set_channel_button($main::currentchannel) if ($main::currentchannel);
+    $main::config{$main::currentchannel}{'button'}
+      ->SetLabel("$main::currentchannel")
+	if ($main::currentchannel);
+    if ($main::locked && $main::currentchannel eq $channelb->{'channel'}) {
+	# unlock
+	$main::locked = 0;
+    } else {
+	# lock to a channel
+	main::set_channel($channelb->{'channel'});
+	set_channel_button($channelb->{'channel'}, wxNORMAL, wxBOLD);
+	$channelb->SetLabel("(L) $channelb->{'channel'}");
+	$main::locked = 1;
+    }
+
+}
+
+sub OnQuit {
+    exit;
+}
+
+sub OnEnable {
+    my ($channel) = $_[0];
+    if ($main::config{$channel}{'enabled'} eq 'false') {
+	$main::config{$channel}{'enabled'} = 'true';
+	set_channel_button($channel);
+	$main::config{$channel}{'button'}->SetLabel("$channel");
+    } else {
+	$main::config{$channel}{'enabled'} = 'false';
+	set_channel_button($channel, wxSLANT);
+	$main::config{$channel}{'button'}->SetLabel("(D) $channel");
+    }
+}
+
+sub set_channel_button {
+    my ($channel, $ifont, $bfont) = @_;
+    $ifont = wxNORMAL if (!$ifont);
+    $bfont = wxNORMAL if (!$bfont);
+    my $font = Wx::Font->new( 8, wxROMAN, $ifont, $bfont);
+    $main::config{$channel}{'button'}->SetFont($font);
 }
 
 sub new {
@@ -42,6 +80,18 @@ sub new {
    my $panel = Wx::Panel->new($this, -1);
    my $grid = new Wx::GridSizer(1,int(($#main::toscan + 3)/2));
 
+   #
+   # MENU setup
+   #
+   my($MENU_QUIT) = (3000..3999);
+   my($mfile) = Wx::Menu->new(undef, wxMENU_TEAROFF);
+   $mfile->Append($MENU_QUIT, "&Quit\tCtrl-Q", "Quit this program");
+
+   my($mbar) = Wx::MenuBar->new();
+   $mbar->Append($mfile, "&Test");
+   $this->SetMenuBar($mbar);
+   EVT_MENU($this, $MENU_QUIT, \&OnQuit);
+
    my $channelid = 2000;
    foreach my $channel (@main::toscan) {
        $main::config{$channel}{'button'} =
@@ -54,6 +104,15 @@ sub new {
 		  \&channel_button);
        $channelid++;
    }
+
+   my $enableid = 4000;
+   my($menable) = Wx::Menu->new(undef, wxMENU_TEAROFF);
+   foreach my $channel (@main::toscan) {
+       $menable->Append($enableid, "$channel", "");
+       EVT_MENU($this, $enableid, sub {OnEnable($channel);});
+       $enableid++;
+   }
+   $mbar->Append($menable, "&Enable/Disable");
 
    $grid->Add($lockbutton = Wx::Button->new($panel, 1, 'Lock            '));
    $this->{'lockbutton'} = $lockbutton;
@@ -99,19 +158,23 @@ sub new {
 }
 
 sub on_timer {
-    if ($main::currentchannel) {
-        my $font = Wx::Font->new( 8, wxROMAN, wxNORMAL, wxNORMAL);
-        $main::config{$main::currentchannel}{'button'}->SetFont($font);
-    }
+    my $oldchannel = $main::currentchannel;
     my $sleeptime = main::next_scan();
-    if ($main::currentchannel) {
-        my $font = Wx::Font->new( 8, wxROMAN, wxNORMAL, wxBOLD);
-        $main::config{$main::currentchannel}{'button'}->SetFont($font);
+    if ($main::currentchannel ne $oldchannel) {
+	if ($oldchannel) {
+	    my $font = Wx::Font->new( 8, wxROMAN, wxNORMAL, wxNORMAL);
+	    $main::config{$oldchannel}{'button'}->SetFont($font);
+	}
+	if ($main::currentchannel) {
+	    my $font = Wx::Font->new( 8, wxROMAN, wxNORMAL, wxBOLD);
+	    $main::config{$main::currentchannel}{'button'}->SetFont($font);
+	}
+	$lockbutton->SetLabel(($main::locked ? "Unlock" : "Lock") . " $main::currentchannel");
     }
-    $lockbutton->SetLabel(($main::locked ? "Unlock" : "Lock") . " $main::currentchannel");
-   my $timer = Wx::Timer->new($mainpanel, 1000);
-   $timer->Start($sleeptime * 1000, 1);
-   $mainpanel->EVT_TIMER($timer, \&on_timer);
+    my $timer = Wx::Timer->new($mainpanel, 1000);
+    $timer->Start(($sleeptime > .01 ? $sleeptime : .01) * 1000, 1);
+    $mainpanel->EVT_TIMER($timer, \&on_timer);
+    $mainpanel->Update();
 }
 
 
